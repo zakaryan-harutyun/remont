c5036550117db3a0431bf93a3b6cf20f31f7d616
Liketodev2 <liketodev2@gmail.com>
2022-02-15 12:37:35 +0400

    categories

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -4,9 +4,9 @@
 @extends('layouts.main')
 @section('content')
     <div class="container">
-        <div>
+        <div style="margin-top: 50px">
             @foreach($services as $service)
-                    <div style="padding:10px; border:1px solid black">
+                    <div style="padding:10px;margin: 10px; border:1px solid black">
                         <div>{{$service->title}}</div>
                         <div>{{$service->description}}</div>
                     </div>
